56e3546c2bcada5e04fe4e62f40107f4491f77f3
Nabil Kashyap <n@nabilk.com>
2015-04-29 13:56:13 -0400

    changed to site.conf, added uwsgi params, beginning changes to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,26 +1,27 @@
 #/usr/bin/env bash
 
-INSTALL=(  "mysql-server"
-           "php5-fpm"
-           "php5-mysql"
-      	   "nginx"
-           "git" 
-           "imagemagick" 
-           "curl"
-           "htop"
-           "unzip" )
+APTGET_INSTALL=(  "mysql-server"
+      	          "nginx"
+                  "git" 
+                  "curl"
+                  "htop"
+                  "unzip" )
 
+PIP_INSTALL=( "Django"
+              "pillow"
+              "uwsgi" )
+           
 apt-get -y update
 
-for i in "${INSTALL[@]}"
+for i in "${APTGET_INSTALL[@]}"
 do
 
     echo "apt-get -y install ${i}"
     if [ "${i}" = "mysql-server" ]
     then
         echo " ******* set selections ******** "
-        debconf-set-selections <<< 'mysql-server mysql-server/root_password password omekaDev'
-        debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password omekaDev'
+        debconf-set-selections <<< 'mysql-server mysql-server/root_password password djangoDev'
+        debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password djangoDev'
     fi    
     apt-get -y install "${i}"
 done
@@ -30,26 +31,8 @@ echo "sed **********************"
  service php5-fpm restart
 
 echo "nginx configure **********************"
- mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
- cp /vagrant/default /etc/nginx/sites-available/default
+ ln -s /vagrant/site.conf etc/nginx/sites-enabled/
 
 
 echo "start services **********************"
  service nginx restart
-
-echo "wget omeka"
- wget https://github.com/omeka/Omeka/archive/master.zip -P /vagrant/webroot/
- unzip /vagrant/webroot/master.zip -d /vagrant/webroot/
- mv /vagrant/webroot/Omeka-master/* /vagrant/webroot/
- rm -r /vagrant/webroot/Omeka-master
- rm /vagrant/webroot/db.ini.changeme
- cp /vagrant/db.ini /vagrant/webroot/
- rm /vagrant/webroot/application/config/config.ini.changeme
- cp /vagrant/config.ini /vagrant/webroot/application/config/
- mv /vagrant/webroot/application/config/logs/errors.log.empty /vagrant/webroot/application/config/logs/errors.log
- chmod -R 775 /vagrant/webroot/application/logs
- cp /vagrant/.htaccess /vagrant/webroot/
-
-echo "mysql setup"
-mysql -u root -pomekaDev --execute="CREATE DATABASE IF NOT EXISTS omeka CHARACTER SET utf8 COLLATE utf8_unicode_ci";
-mysql -u root -pomekaDev --execute="GRANT ALL PRIVILEGES ON omeka.* TO 'omeka'@'localhost' IDENTIFIED BY 'omeka'";
